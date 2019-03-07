ee4f756dbb3e5a3f93f55623b71f1342bcfd21b0
Laura <laura.s.chavez@outlook.com>
2019-03-07 14:53:20 -0500

    remove duplications first version working empty errors and moving validations

--- script.js
+++ script.js
@@ -4,21 +4,25 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
 
     for (var errorMessage of document.querySelectorAll(".input-hint")) {
         errorMessage.remove();
-        var invalidInputs = [];
+        
     }
-// review inputs
+// review inputs and clean messages
     var inputDivs = document.getElementsByClassName("input-field");
         for (var inputDiv of inputDivs) {
             inputDiv.classList.remove("input-invalid");
             inputDiv.classList.remove("input-valid");
 
             // validate input if "! false" move to invalidInputs
+            var invalidInputs = [];
             for (var input of inputDiv.getElementsByTagName("input")) {
+                
                 if (!validateInput(input)) {
-                    invalidInputs.push(input);
+                    invalidInputs.push(input);  
                 }
             }
-// if errors send message to screen
+            
+            
+// if errors send message to screen assign classlist valid-invalid
             if (invalidInputs.length > 0) {
                 showEmptyNameErrorname(inputDiv, invalidInputs);
             } else {
@@ -51,18 +55,17 @@ function validateInput(input) {
     if (inputValue === "") {
         return false;
     }
-
     switch (input.id) {
         case "name":
             return true;
         case "car-year":
-            return validatecar(inputValue);
+            return validacar(inputValue);
         case "car-make":
             return true;
         case "car-model":
             return true;
         case "start-date":
-            return validadate();
+            return validadate(inputValue);
         case "days":
             return validnumber(inputValue);
         case "credit-card":
@@ -102,57 +105,35 @@ function getInputName(input) {
 }
 
     
-
-
-
-//valida CVV must be a three-digit number
-function validcvv(field, divname, cvv2) {
-    var Max_Length = 3;
-    var length = document.getElementById("cvv").value.length;
-    var error = false;
-    if (isNaN(cvv2)) {
-        error = true
-    } else {
-        if (length == Max_Length) {
+// validate car-year field
+function validacar(inputValue) {
+    
+    // Returns true."ABC" if not number send error
+    if (isNaN(inputValue)) {return false}
+    // 
+    else {
+        var fix = 1900;
+        var cdate = new Date();
+        var cyear = cdate.getFullYear();
+        // valida year >1990 and future if not send send error
+        if ((caryear2 > fix) && (caryear2 <= cyear)) {
             document.getElementById(divname).classList.add("input-valid");
-        } else {
-            error = true
         }
+        //if no valid send message
+        else { return false}
     }
-    if (error) {
-        var errorDiv = document.createElement('div')
-    errorDiv.classList.add('error-msg')
-    var message = 'CVV MUST TO BE 3 NUMBERS'
-    errorDiv.innerText = message
-    var field = document.getElementById('cvv')
-    field.parentElement.appendChild(errorDiv)
-    field.classList.add('&.alert-lg')
-    document.getElementById(divname).classList.add("input-invalid");
-    return fall = false;
-    }
-};
-//validate number of days number and between 1-30
-function validnumber(field, days2, divname) {
-    error = false;
-    if (isNaN(days2)) {error = true;
-
-    } else {
-        if (days2 >= 1 && days2 <= 30) {
-            document.getElementById(divname).classList.add("input-valid");
-        } else {error = true}
-
-    }
-    if (error) {
-        var errorDiv = document.createElement('div')
-    errorDiv.classList.add('error-msg')
-    var message = 'NUMBER OF DAYS MUST BE BETWEEN 1-30'
-    errorDiv.innerText = message
-    var field = document.getElementById('cvv')
-    field.parentElement.appendChild(errorDiv)
-    field.classList.add('&.alert-lg')
-    document.getElementById(divname).classList.add("input-invalid");
-    return fall = false;
-    }
+    // if (error) {
+    //     var errorDiv = document.createElement('div')
+    // errorDiv.classList.add('error-msg')
+    // var message = 'CAR YEAR NEEDS TO BE AFTER 1990'
+    // errorDiv.innerText = message
+    // var field = document.getElementById('car-year')
+    // field.parentElement.appendChild(errorDiv)
+    // field.classList.add('&.alert-lg')
+    // document.getElementById(divname).classList.add("input-invalid");
+    // return fall = false;} else {
+    //     document.getElementById(divname).classList.add("input-valid");
+    // }
 };
 
 // validate DateParking 
@@ -197,36 +178,79 @@ function validadate(field, startdate2, divname) {
         document.getElementById(divname).classList.add("input-valid");
     }
 };
-// validate car-year field
-function validacar(divname, caryear2) {
-    var error = false
-    // Returns true."ABC" if not number send error
-    if (isNaN(caryear2)) {error = true}
-    // 
-    else {
-        var fix = 1900;
-        var cdate = new Date();
-        var cyear = cdate.getFullYear();
-        // valida year >1990 and future if not send send error
-        if ((caryear2 > fix) && (caryear2 <= cyear)) {
+//validate number of days number and between 1-30
+function validnumber(field, days2, divname) {
+    error = false;
+    if (isNaN(days2)) {error = true;
+
+    } else {
+        if (days2 >= 1 && days2 <= 30) {
             document.getElementById(divname).classList.add("input-valid");
-        }
-        //if no valid send message
-        else { error = true}
+        } else {error = true}
+
     }
     if (error) {
         var errorDiv = document.createElement('div')
     errorDiv.classList.add('error-msg')
-    var message = 'CAR YEAR NEEDS TO BE AFTER 1990'
+    var message = 'NUMBER OF DAYS MUST BE BETWEEN 1-30'
     errorDiv.innerText = message
-    var field = document.getElementById('car-year')
+    var field = document.getElementById('cvv')
     field.parentElement.appendChild(errorDiv)
     field.classList.add('&.alert-lg')
     document.getElementById(divname).classList.add("input-invalid");
-    return fall = false;} else {
-        document.getElementById(divname).classList.add("input-valid");
+    return fall = false;
     }
 };
+// ////credit card validation
+function validateCardNumber(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
+};
+//valida CVV must be a three-digit number
+function validcvv(field, divname, cvv2) {
+    var Max_Length = 3;
+    var length = document.getElementById("cvv").value.length;
+    var error = false;
+    if (isNaN(cvv2)) {
+        error = true
+    } else {
+        if (length == Max_Length) {
+            document.getElementById(divname).classList.add("input-valid");
+        } else {
+            error = true
+        }
+    }
+    if (error) {
+        var errorDiv = document.createElement('div')
+    errorDiv.classList.add('error-msg')
+    var message = 'CVV MUST TO BE 3 NUMBERS'
+    errorDiv.innerText = message
+    var field = document.getElementById('cvv')
+    field.parentElement.appendChild(errorDiv)
+    field.classList.add('&.alert-lg')
+    document.getElementById(divname).classList.add("input-invalid");
+    return fall = false;
+    }
+};
+
 // expiration validation
 function validaexpiration(divname, expiration2) {
     var validexpiration = false;
@@ -269,76 +293,4 @@ function validaexpiration(divname, expiration2) {
     return fall = false;} else {
         document.getElementById(divname).classList.add("input-valid");
     }
-};
-
-// ////credit card validation
-function validateCardNumber(number) {
-    var regex = new RegExp("^[0-9]{16}$");
-    if (!regex.test(number))
-        return false;
-
-    return luhnCheck(number);
-}
-
-function luhnCheck(val) {
-    var sum = 0;
-    for (var i = 0; i < val.length; i++) {
-        var intVal = parseInt(val.substr(i, 1));
-        if (i % 2 == 0) {
-            intVal *= 2;
-            if (intVal > 9) {
-                intVal = 1 + (intVal % 10);
-            }
-        }
-        sum += intVal;
-    }
-    return (sum % 10) == 0;
-};
-///clean total
-var divname = document.getElementById('total')
-field.classList.remove('total')
-var message = ""
-divname.innerText = message
-}
-return fall = true;
-}
-
-// total calculation and message
-function ftotal(startdate2, days2) {
-
-
-    var total = "";
-    var i = days2;
-    var ddate = new Date(startdate2);
-    ddate.setDate(ddate.getDate() + 1);
-    var total1 = 0
-    var total2 = 0
-
-    do {
-
-        var dyear = ddate.getFullYear();
-        var dmonth = (1 + ddate.getMonth()).toString();
-        dmonth = dmonth.length > 1 ? dmonth : '0' + dmonth;
-        var dday = ddate.getDate().toString();
-        var dow = ddate.getDay()
-
-        if (dow == 0 || dow == 6) {
-            total1 = 7 + total1
-        } else if (dow !== '') {
-            total2 = 5 + total2
-        };
-
-
-        ddate.setDate(ddate.getDate() + 1);
-        i = i - 1;
-
-    } while (i >= 0);
-    var dow = 0
-    dow = total1 + total2;
-    ///////////message to screen
-    var divname = document.getElementById('total')
-    divname.classList.add('total')
-    var message = "Total:" + ' ' + dow
-    divname.innerText = message
-
 };
